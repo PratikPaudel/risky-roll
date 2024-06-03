f65d889a41cc92b75347722de6de855c9513320d
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-06-02 22:14:38 -0500

    added functions

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'script'
+const totalScoreEl = document.querySelector('.total-score').textContent = "hi";
+const currentScoreEl = document.querySelector('.current-score').textContent = "hello";
+const btnHold = document.querySelector('.hold'); 
+let diceValue;
+let totalScore;
+let currentScore = 0;
+const rollDiceEl = document.querySelector(".roll-dice");
+rollDiceEl.addEventListener('click', function () {
+    let diceValue= Math.floor(Math.random()*6)+1;
+    let diceImage = document.querySelector('.diceImage');
+    diceImage.src = `dice-${diceValue}.png`;
+    currentScore = currentScore + diceValue;
+    console.log(currentScore);
+    if (btnHold.addEventListener('click', () => {
+        totalScore += currentScore;
+    }));
+}); 
