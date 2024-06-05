82cf935f8e5230df046afffc2a5f474d5f0e602c
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-06-04 22:11:30 -0500

    updating function

--- script.js
+++ script.js
@@ -5,15 +5,26 @@ const btnHold = document.querySelector('.hold');
 let diceValue;
 let totalScore;
 let currentScore = 0;
+const playerScoreArray = [0, 0];
 const rollDiceEl = document.querySelector(".roll-dice");
 rollDiceEl.addEventListener('click', function () {
-    let diceValue= Math.floor(Math.random()*6)+1;
     let diceImage = document.querySelector('.diceImage');
-     diceImage.classList.remove('hidden');
+    diceImage.classList.remove('hidden');
+    let diceValue= Math.floor(Math.random()*6)+1;
     diceImage.src = `dice-${diceValue}.png`;
     currentScore = currentScore + diceValue;
     console.log(currentScore);
+    if (diceValue === 1) {
+        switchPlayer();
+        console.log("You got 1");
+    }
     if (btnHold.addEventListener('click', () => {
         totalScore += currentScore;
+        playerScoreArray[0] = totalScore;
+        console.log(playerScoreArray[0]);
+        switchPlayer();
     }));
 }); 
+function switchPlayer() {
+
+}
