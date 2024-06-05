64d2e9607b08801aeac2d05ab345277e2891a430
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-06-04 21:59:32 -0500

    updated rolldice function

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const rollDiceEl = document.querySelector(".roll-dice");
 rollDiceEl.addEventListener('click', function () {
     let diceValue= Math.floor(Math.random()*6)+1;
     let diceImage = document.querySelector('.diceImage');
+     diceImage.classList.remove('hidden');
     diceImage.src = `dice-${diceValue}.png`;
     currentScore = currentScore + diceValue;
     console.log(currentScore);
