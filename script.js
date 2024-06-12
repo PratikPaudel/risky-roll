05df2c0806486538619256377c3272e5f221f793
Pratik Paudel <pratik.paudel@nwciowa.edu>
2024-06-11 22:06:28 -0500

    .

--- script.js
+++ script.js
@@ -4,25 +4,31 @@ let diceValue;
 let currentScore = 0;
 const playerScoreArray = [0, 0];
 const rollDiceEl = document.querySelector(".roll-dice");
+let diceImage = document.querySelector('.diceImage');
+
 let activePlayer = 0;
 
 const updateScores = function () {
     const playerScoreEl0 = document.getElementById(`score--${activePlayer}`).textContent= playerScoreArray[activePlayer];
 }
+
 const updateBackground = function (activePlayer) {
     document.querySelector(`.player--${activePlayer}`).classList.toggle('player--active');
+    console.log(activePlayer + "hiii");
 }
+
 const winnerWinnerChickenDinner = function () {
-    document.querySelector(`.player--${activePlayer}`).classList.toggle('player--winner');
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
 }
+
 rollDiceEl.addEventListener('click', function () {
-    let diceImage = document.querySelector('.diceImage');
     diceImage.classList.remove('hidden');
     const diceValue= Math.floor(Math.random()*6)+1;
     diceImage.src = `dice-${diceValue}.png`;
     diceValue !== 1 
     ? (currentScore += diceValue, document.getElementById(`current--${activePlayer}`).textContent = currentScore)
     : switchPlayer();
+    updateBackground(activePlayer);
     });
 
 const switchPlayer = function() {
@@ -30,6 +36,7 @@ const switchPlayer = function() {
     updateScores(activePlayer);
     currentScore = 0;
     document.getElementById(`current--${activePlayer}`).textContent = 0; 
+    updateBackground(activePlayer);
     activePlayer = 1 - activePlayer;
 }
 
